aaad3e50465a99bd675c3f836145bffe600d8cc7
Cory Monty <code@getbraintree.com>
2011-07-19 21:36:00 -0500

    Adds Rails App for Devise

---
+++ rails3_tr_devise/config/initializers/braintree.rb
@@ -0,0 +1,4 @@
+Braintree::Configuration.environment = :development
+Braintree::Configuration.merchant_id = "integration_merchant_id"
+Braintree::Configuration.public_key = "integration_public_key"
+Braintree::Configuration.private_key = "integration_private_key"
---
+++ rails3_tr_devise/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3TrDevise::Application.config.secret_token = 'fd97a9f6cf79faaa45dba75615d676fd0717aace1ecf2d10bd37bae13ec039c0fe216de79599293ff1a9da3d4bb80edcfb23a16fd85b142cef4931745d2be1dd'

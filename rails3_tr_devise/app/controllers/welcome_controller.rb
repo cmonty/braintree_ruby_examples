ca0d686700af042ef37c3655da5dde315026921e
Cory Monty <cory.monty@gmail.com>
2011-07-22 17:10:00 -0500

    Elminates Webrat and uses Capybara

--- rails3_tr_devise/app/controllers/welcome_controller.rb
+++ rails3_tr_devise/app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+
+  def index
+    if not user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
 end
--- rails3_tr_devise/spec/controllers/welcome_controller_spec.rb
+++ rails3_tr_devise/spec/controllers/welcome_controller_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 
 describe WelcomeController do
 
+  it "should redirect to Sign Up page" do
+    get :index
+
+    page.has_content? 'Sign in'
+  end
+
+  it "should display product information" do
+    get :index
+
+    page.has_table? 'product', :rows => [['FooBar', '100']]
+    page.has_content? 'Products'
+  end
 end

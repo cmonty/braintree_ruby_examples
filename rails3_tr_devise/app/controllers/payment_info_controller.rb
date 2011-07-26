501db01314ed920f46b1f59c4532d24af8168dff
Cory Monty <cory.monty@gmail.com>
2011-07-26 17:02:41 -0500

    Passing test for adding new payment info

--- rails3_tr_devise/app/controllers/payment_info_controller.rb
+++ rails3_tr_devise/app/controllers/payment_info_controller.rb
@@ -4,4 +4,17 @@ class PaymentInfoController < ApplicationController
   def new
     @tr_data = Braintree::TransparentRedirect.create_customer_data(:redirect_url => confirm_payment_info_url)
   end
+
+  def confirm
+   @result = Braintree::TransparentRedirect.confirm(request.query_string)
+
+   if @result.success?
+    current_user.braintree_customer_id = @result.customer.id
+    current_user.save!
+    render :action => "confirm"
+   else
+    render :action => "new"
+   end
+
+  end
 end
--- rails3_tr_devise/spec/controllers/payment_info_controller_spec.rb
+++ rails3_tr_devise/spec/controllers/payment_info_controller_spec.rb
@@ -13,5 +13,14 @@ describe PaymentInfoController do
     page.fill_in 'customer_credit_card_expiration_date', :with => '01/25'
     page.fill_in 'customer_credit_card_cvv', :with => '400'
     page.click_button 'Save Payment Info'
+
+    page.should have_content('Payment Info Confirmation')
+    page.should have_content('411111******1111')
+  end
+
+  it 'should display form again because of validation errors' do
+    sign_in_as_user
+
+    visit new_payment_info_path
   end
 end

4b73f4fd6a1868e15535e285bb327060c14dccdd
Cory Monty <cory.monty@gmail.com>
2011-08-02 17:15:56 -0500

    Only attaches_braintree_data when the user has braintree_customer_id

--- rails3_tr_devise/app/controllers/welcome_controller.rb
+++ rails3_tr_devise/app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     redirect_to new_user_session_url unless user_signed_in?
-    current_user.with_braintree_data!
+    current_user.with_braintree_data! if current_user.has_payment_info?
     @products = Product.all
   end
 end

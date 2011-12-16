c852d9150a7050460fe6f84d16b6105e92bb9929
Cory Monty <code@getbraintree.com>
2011-12-16 12:45:39 -0600

    Removes defining class methods on User, replaces with instance methods

--- rails3_tr_devise/app/models/user.rb
+++ rails3_tr_devise/app/models/user.rb
@@ -1,42 +1,32 @@
-module BraintreeUser
-
-  FIELDS = [:first_name, :last_name, :phone, :website, :company, :email, :fax, :addresses, :credit_cards, :custom_fields]
-
-  def self.attach_braintree_data(user)
-    braintree_data = Braintree::Customer.find(user.braintree_customer_id)
-
-    FIELDS.each do |field|
-      user.class.send(:define_method, field) { braintree_data.send(field) }
-    end
-  end
-
-end
-
 class User < ActiveRecord::Base
+  FIELDS = [:first_name, :last_name, :phone, :website, :company, :fax, :addresses, :credit_cards, :custom_fields]
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
+  attr_accessor *FIELDS
   attr_accessible :email, :password, :password_confirmation, :remember_me, :braintree_customer_id
 
-  include BraintreeUser
-
   def has_payment_info?
-    !!self.braintree_customer_id
+    !!braintree_customer_id
   end
 
   def with_braintree_data!
-    return self unless self.has_payment_info?
+    return self unless has_payment_info?
+    braintree_data = Braintree::Customer.find(braintree_customer_id)
 
-    BraintreeUser.attach_braintree_data(self)
+    FIELDS.each do |field|
+      send(:"#{field}=", braintree_data.send(field))
+    end
     self
   end
 
   def default_credit_card
-    return nil unless self.has_payment_info?
+    return unless has_payment_info?
 
-    self.credit_cards.find { |cc| cc.default? }
+    credit_cards.find { |cc| cc.default? }
   end
 end
